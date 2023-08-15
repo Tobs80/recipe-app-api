5ca41f08a224ac37bc9d8ea31800f45edc80c4f2
Martin Tobar <martoflo99@gmail.com>
2023-08-15 14:13:29 -0500

    yeah

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe,Tag
+from core.models import Recipe,Tag, Ingredient
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -31,7 +31,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
         
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+                 mixins.ListModelMixin, viewsets.GenericViewSet):
     """Manage tags in the database"""
 
     serializer_class = serializers.TagSerializer
@@ -42,5 +43,15 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         """Filter queryset to authenticate user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+        
 
-         
+class IngredientViewSet(mixins.DestroyModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database"""
+    serialiezr_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
